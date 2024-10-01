d80086241a36c8a103f9d2055ca3711e900d116e
mikalueh <mikabenlue@gmail.com>
2024-10-01 06:45:52 +0200

    added tcpdump to path

--- common/service.sh
+++ common/service.sh
@@ -6,5 +6,15 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# This script will be executed in post-fs-data mode
+# This script will be executed in late_start service mode
+
+# wait for boot to complete
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+    sleep 1
+done
+
+# ensure boot has actually completed
+sleep 5
+
+# restart on crash
 export PATH=/system/bin:$PATH
